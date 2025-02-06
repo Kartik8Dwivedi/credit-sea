feaf315269287c59d818a40054c661b0ae097082
Kartik Dwivedi <codewithme.kartik@gmail.com>
2025-02-06 17:47:30 +0530

    code structure for frontend complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UploadPage from "./pages/UploadPage";
+import ReportsPage from "./pages/ReportsPage";
+import ReportDetail from "./pages/ReportDetail";
+import Navbar from "./components/Navbar";
 
+function App() {
   return (
-    <>
-      <div>Hello There!</div>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/reports" element={<ReportsPage />} />
+          <Route path="/reports/:id" element={<ReportDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
